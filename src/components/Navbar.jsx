d6d5670dcc7f4041f77c233025b59306c08c9b51
Jun0613-spec <kumo5110@gmail.com>
2023-01-18 11:54:20 +0000

    portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div className=" cursor-pointer">
         <Link to="home" smooth={true} duration={500}>
-          <img src={Logo} alt="Logo Image" style={{ width: "45px" }} />
+          <image src={Logo} alt="Logo Image" style={{ width: "45px" }} />
         </Link>
       </div>
 
